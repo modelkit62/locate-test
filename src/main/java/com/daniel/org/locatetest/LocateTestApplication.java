4dd3c717233487536ca670e426d94acab2d14991
d r <modelkit62@gmail.com>
2020-10-31 11:54:32 +0000

    Initial commit

---
+++ src/main/java/com/daniel/org/locatetest/LocateTestApplication.java
@@ -0,0 +1,14 @@
+package com.daniel.org.locatetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocateTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocateTestApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/daniel/org/locatetest/LocateTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.org.locatetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocateTestApplicationTests {
+
+	/*@Test
+	void contextLoads() {
+	}*/
+
+}
